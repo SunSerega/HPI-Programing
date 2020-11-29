cc0e4b3e62080d1b9476c82b401e846449427502
SunSerega <latchenko3@yandex.ru>
2020-11-29 16:50:52 +0200

    Coding part done

--- ЛР 8/ЛР 8.cpp
+++ ЛР 8/ЛР 8.cpp
@@ -2,7 +2,15 @@
 #include <io.h>
 #include <fcntl.h>
 
-//ToDo Другие заголовки
+using namespace std;
+
+const int arr_len = 200;
+const int chain_len = 10;
+
+inline int Random(int x, int y)
+{
+    return rand() % (y - x + 1) + x;
+}
 
 int main()
 {
@@ -10,6 +18,42 @@ int main()
     if (_setmode(_fileno(stdin ), _O_U16TEXT) == -1) return -1;
     if (_setmode(_fileno(stderr), _O_U16TEXT) == -1) return -1;
 
-    //ToDo Тело программы
+    int arr[arr_len];
+    for (auto i = 0; i < arr_len; ++i)
+        arr[i] = Random(0, 100); // Можно и rand() % 101, но так код более читабельный //ToDo в отчёт
 
+    for (auto i = 0; i < arr_len; ++i)
+        wcout << arr[i] << ' ';
+    wcout << endl;
+
+    int chain[chain_len];
+    copy(arr, arr+10, chain);
+
+    int sum = 0;
+    for (auto i = 0; i < chain_len; ++i)
+        sum += chain[i];
+
+    int max_sum = sum;
+    int max_sum_ind = chain_len - 1;
+    for (auto i = chain_len; i < arr_len; ++i)
+    {
+        auto chain_ind = i % chain_len;
+        sum -= chain[chain_ind];
+        auto val = arr[i];
+        sum += val;
+        if (sum > max_sum)
+        {
+            max_sum = sum;
+            max_sum_ind = i;
+        }
+        chain[chain_ind] = val;
+    }
+
+    wcout << L"Участок с максимальной суммой:" << endl;
+
+    for (auto i = max_sum_ind - (chain_len-1); i < max_sum_ind; ++i)
+        wcout << arr[i] << ' ';
+    wcout << endl;
+
+    wcout << L"Имеет сумму: " << max_sum << endl;
 }
--- ЛР 9/ЛР 9.cpp
+++ ЛР 9/ЛР 9.cpp
@@ -1,8 +1,11 @@
 ﻿#include <iostream>
+#include <iomanip>
 #include <io.h>
 #include <fcntl.h>
 
-//ToDo Другие заголовки
+using namespace std;
+
+const int matr_w = 9;
 
 int main()
 {
@@ -10,6 +13,25 @@ int main()
     if (_setmode(_fileno(stdin ), _O_U16TEXT) == -1) return -1;
     if (_setmode(_fileno(stderr), _O_U16TEXT) == -1) return -1;
 
-    //ToDo Тело программы
+    int mtr[matr_w][matr_w];
+
+    int c = 1;
+
+    // Заполнение части под главной диагональю (включительно)
+    for (auto i1 = 0; i1 < matr_w; ++i1)
+        for (auto i2 = 0; i2 <= i1; ++i2)
+            mtr[matr_w - 1 - i2][i1 - i2] = c++;
+
+    // Заполнение части над главной диагональю (НЕ_включительно)
+    for (auto i1 = 1; i1 < matr_w; ++i1)
+        for (auto i2 = 0; i2 < matr_w-i1; ++i2)
+            mtr[matr_w-1-i1 - i2][matr_w-1 - i2] = c++;
+
+    for (auto row = 0; row < matr_w; ++row)
+    {
+        for (auto col = 0; col < matr_w; ++col)
+            wcout << setw(3) << mtr[row][col];
+        wcout << endl;
+    }
 
 }
