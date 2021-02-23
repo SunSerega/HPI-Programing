def604c3a8ca13d30e9dd66ed1ab141ba7c48205
Sun Serega <latchenko3@yandex.ru>
2021-02-24 00:10:34 +0200

    ЛР 1

---
+++ Курс 1 Семестр 2/ЛР 1/ЛР 1.cpp
@@ -0,0 +1,19 @@
+﻿// ЛР 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+void ReverseOtp() {
+    int x;
+    cin >> x;
+    if (!x) return;
+    ReverseOtp();
+    cout << x << ' ';
+}
+
+int main()
+{
+    std::cout << "Enter sequence of integers, using 0 to mark end of sequence:\n";
+    ReverseOtp();
+}
